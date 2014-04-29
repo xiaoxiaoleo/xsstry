c744fe61fe2569b9c8170df3eddb64f9fcc3895a
Qi Huang <pinqy520@gmail.com>
2014-04-30 02:34:39 +0800

    add xssmodel creating. go to HangZhou and see you after 5.1 ^_^

--- mode/models.py
+++ mode/models.py
@@ -6,14 +6,25 @@ import django.utils.simplejson as json
 
 
 class XssModel(models.Model):
-    owner = models.ForeignKey(User)
-    xss_projects = models.ManyToManyField(XssProject)
-    is_public = models.BooleanField()
-    name = models.CharField(max_length = 50)
-    description = models.CharField(max_length = 255)
-    config_params = models.TextField() #json{param:defaultvalue}
-    recv_params = models.TextField() #{number: param}
-    def configparams(self):
-        return json.loads(config_params)
-    def recvparams(self):
-        return json.loads(recv_params)
+	owner = models.ForeignKey(User)
+	xss_projects = models.ManyToManyField(XssProject)
+	is_public = models.BooleanField()
+	name = models.CharField(max_length = 50)
+	description = models.CharField(max_length = 255)
+	config_params = models.TextField() #json{param:defaultvalue}
+	recv_params = models.TextField() #{number: param}
+	def configparams(self):
+		return json.loads(config_params)
+	def recvparams(self):
+		return json.loads(recv_params)
+	def set_configparams(self, config_list):
+		try:
+#			self.config_params = json.dumps(config_list)
+		except:
+			pass
+	def set_recvparams(self, param_list):
+		try:
+#			self.recv_params = json.dumps(param_list)
+		except:
+			pass
+
--- mode/views.py
+++ mode/views.py
@@ -15,13 +15,44 @@ def index(request):
 @login_required
 def detail(request, mode_id):
 	model_list = request.user.xssmodel_set.all()
-  	selected_model = get_object_or_404(XssModel, owner = request.user, pk = mode_id)
-  	return render(
-  		request, 
-  		'mode/detail.html', 
-  		{
-  			'error': False, 
-  			'model_list': model_list, 
-  			'selected_model': selected_model, 
-  		}
-  	)
+	selected_model = get_object_or_404(XssModel, owner = request.user, pk = mode_id)
+	return render(
+		request, 
+		'mode/detail.html', 
+		{
+			'error': False, 
+			'model_list': model_list, 
+			'selected_model': selected_model, 
+		}
+	)
+
+@login_required
+def create(request):
+	if request.method == 'POST':
+		model_name = request.POST.get('name')#if not exist return wrong
+		if not model_name:
+			return render(request, 'mode/new.html', {'error': True})
+		m = XssModel.objects.create(
+			owner = request.user,
+			name = model_name)
+		#Todo_list: move to modify.
+		m.description = request.POST.get('desc')
+		m.set_configparams( config_list = request.POST.getlist('config') )
+		m.set_recvparams( param_list = request.POST.getlist('params') )
+		request.POST.get('ispublic')
+		if request.POST.get('ispublic'):
+			m.is_public = True
+		else:
+			m.is_public = False
+		m.save()
+		return HttpResponseRedirect(reverse('mode.detail', mode_id = m.id))
+	else:
+		return render(request, 'mode/new.html', {'error': False})
+
+@login_required
+def modify(request, mode_id):
+
+
+
+@login_required
+def delete(request, mode_id):
